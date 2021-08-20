036dc4fb8067808d39d8c409af22e2861d81bc54
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-08-20 07:01:15 +0200

    Implemented runnable interfaces to demostarte threads

--- src/com/tinyiko/Main.java
+++ src/com/tinyiko/Main.java
@@ -3,16 +3,34 @@ package com.tinyiko;
 public class Main {
 
     public static void main(String[] args) {
-    //   System.out.println("number of threads: "+ Thread.activeCount());
+//        var instance = new Thread(new RunnableExample());
 
-        ThreadExample thread1 = new ThreadExample();
+        Thread thread1 = new Thread(new RunnableExample());
         thread1.setName("First thread");
         thread1.start();
 
 
-        ThreadExample thread2 = new ThreadExample();
+        Thread thread2 = new Thread(new RunnableExample());
         thread2.setName("Second thread");
         thread2.start();
+
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int i = 1;
+                while (i <= 100){
+                    System.out.println(i + " " + Thread.currentThread().getName());
+                    i++;
+                }
+            }
+        });
+
+        thread3.setName("third thread");
+        thread3.start();
+
+//        thread1.start();
+//        thread2.start();
+//        thread3.start();
 
 
     }
--- src/com/tinyiko/RunnableExample.java
+++ src/com/tinyiko/RunnableExample.java
@@ -1,13 +1,13 @@
 package com.tinyiko;
 
-public class ThreadExample extends Thread {
+public class RunnableExample implements Runnable {
 
 
     @Override
     public void run() {
         int i = 1;
         while (i <= 100){
-            System.out.println(i + " " + this.getName());
+            System.out.println(i + " " + Thread.currentThread().getName());
             i++;
         }
     }
